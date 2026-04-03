8513080fde073ade2e4a1444115944906e21c3f8
karthik <karthik2003cmani@gmail.com>
2026-04-03 17:58:36 +0530

    feat: reworked rank-finder for 2025 ranklist

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 @st.cache_data
 def load_pdf_data():
     rows = []
-    with pdfplumber.open('MCA_2024_RankList.pdf') as pdf:
+    with pdfplumber.open('MCA_2025_RankList.pdf') as pdf:
         for page in pdf.pages:
             text = page.extract_table()
             if text:
@@ -15,11 +15,11 @@ def load_pdf_data():
     return rows
 data_rows = load_pdf_data()
 st.set_page_config(
-    page_title="MCA Rank Finder 2024",
+    page_title="MCA Rank Finder 2025",
     page_icon="icon.png", 
     layout="centered"
 )
-st.title("MCA Rank Finder 2024")
+st.title("MCA Rank Finder 2025")
 query = st.text_input("Search by Name / Roll Number").upper().strip()
 matches = []
 name = query
@@ -36,19 +36,21 @@ if name:
         st.error("Sorry, your Name / Roll Number is not found in the list.")
     elif len(matches) == 1:
         st.markdown(f'### Hi, {matches[0][3].title()}!')
-        st.caption(f"Roll Number: {matches[0][2]} | Application Number: {matches[0][1]} | Date of Birth: {matches[0][4]}")
+        st.caption(f"Roll Number: {matches[0][2]} | Application Number: {matches[0][1]} | Date of Birth: {matches[0][7]}")
         col1, col2 = st.columns([1,1])
         col1.metric("Rank", f"{matches[0][0]}")
-        col2.metric("Score ", f"{matches[0][5]}/120")
+        col2.metric("Score ", f"{matches[0][8]}/120")
         st.divider()
         st.markdown("#### Subject Breakdown")
         col1, col2 = st.columns([1,1])
-        col1.metric(f"Computer Science", f"{matches[0][6]}/50")
-        col2.metric(f"Mathematics and Statistics", f"{matches[0][7]}/25")
+        col1.metric(f"Computer Science", f"{matches[0][-4]}/50")
+        col2.metric(f"Mathematics and Statistics", f"{matches[0][-3]}/25")
         col3, col4 = st.columns([1,1])
-        col3.metric(f"Quantitative Aptitude, Logical Ability", f"{matches[0][8]}/25")
-        col4.metric(f"English & G.K", f"{matches[0][9]}/20")
+        col3.metric(f"Quantitative Aptitude, Logical Ability", f"{matches[0][-2]}/25")
+        eng_gk = int(matches[0][-5]) - int(matches[0][-4]) - int(matches[0][-3]) - int(matches[0][-2])
+        col4.metric(f"English & G.K", f"{eng_gk-6}/20*")
+        st.caption("*English & G.K marks are estimated by subtracting CS, Math, Aptitude, and a fixed bonus of 6 marks (awarded to all students due to out-of-syllabus questions).")
     else:
         st.info("Multiple entries found. Please find your Roll Number below:")
-        df_matches = pd.DataFrame(matches, columns=["Rank", "App No", "Roll No", "Name", "DOB", "Total", "CS", "Math", "Apt", "Eng", "Final Rank"])
+        df_matches = pd.DataFrame(matches, columns=["Rank", "App No", "Roll No", "Name", "Nativity", "Community", "Whether OEC", "DOB", "Total", "CS", "Math", "Apt", "Final Rank"])
         st.table(df_matches[["Rank", "Roll No", "Name", "Total"]]) 
